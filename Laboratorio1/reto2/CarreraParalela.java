61e8a103d9476332cfc2a5d8ad5120df57d7d07a
MissGomezzz <sofiangelagv@gmail.com>
2026-02-03 21:35:25 -0500

    calculando mínimo y cantidad de datos

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -11,10 +11,41 @@ public class CarreraParalela {
                 .orElseThrow();
     }
 
+    private static int calcularMinimo(List<Integer> numeros) {
+        return numeros.stream()
+                .mapToInt(Integer::intValue)
+                .min()
+                .orElseThrow();
+    }
+
+    private static int cantidadNumeros(List<Integer> numeros) {return numeros.size();}
+
+    public static class Resultado {
+        public int minimo;
+        public int cantidad; 
+
+        public Resultado (int minimo, int cantidad) {
+            this.minimo = minimo;
+            this.cantidad = cantidad;
+
+        }
+    }
+
+    public static Resultado calcularMinResultado (List<Integer> numeros) {
+        int minimo = calcularMinimo(numeros);
+        int cantidad = cantidadNumeros(numeros);
+        return new Resultado (minimo, cantidad);
+    }
+
     public static void main(String[] args) {
         List<Integer> numeros = List.of(3, 7, 2, 9, 5);
         int maximo = calcularMaximo(numeros);
         System.out.println("El número máximo es: " + maximo);
+        Resultado resultado = calcularMinResultado(numeros);
+
+        System.out.println(String.format(
+            "El número mínimo es: %d, y la cantidad de datos es %d", resultado.minimo, resultado.cantidad));
+        
     }
-    
 }
+    
