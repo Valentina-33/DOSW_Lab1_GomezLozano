92067dd1db4ab4aa55e2e0f16740ac54ad22f968
MissGomezzz <sofiangelagv@gmail.com>
2026-02-05 16:59:46 -0500

    reto 3 FINAL

--- Laboratorio1/reto3/Reto3.java
+++ Laboratorio1/reto3/Reto3.java
@@ -1,26 +1,34 @@
 package Laboratorio1.reto3;
 
+import java.util.stream.IntStream;
+
 public class Reto3 {
+    public static String repetirTresVeces(String mensaje) {
+        String resultado = IntStream.range(0,3)
+                .mapToObj(i -> mensaje)
+                .collect(() -> new StringBuilder(), (r,s) -> {
+                    if (!r.isEmpty()) r.append(" ");
+                    r.append(s);
+                },
+                (sb1, sb2) -> sb1.append(sb2))
+                .toString();
+
+        return resultado.trim();
+    }
+    
+
     public static String invertir(String mensaje) {
         StringBuffer resultado = new StringBuffer(mensaje);
         return resultado.reverse().toString();
+
+    }
+
+    public static String particularEco(String mensaje) {
+        return invertir(repetirTresVeces(mensaje));
     }
 
     public static void main(String[] args) {
-        System.out.println(invertir("ana banana"));
-        System.out.println(repetirTresVeces("Hola"));
-
-    }
-
-    public static String repetirTresVeces(String mensaje) {
-        StringBuilder resultado = new StringBuilder();
-        Runnable repetir = () -> resultado.append(mensaje).append(" ");
-
-        repetir.run();
-        repetir.run();
-        repetir.run();
-
-        return resultado.toString().trim();
+        System.out.println(particularEco("Repetir"));
     }
 }
 
