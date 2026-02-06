f89ba6c96686c6a9e42c8a53ffde8abce258f934
MissGomezzz <sofiangelagv@gmail.com>
2026-02-05 20:46:19 -0500

    reto4 parte Ángela

--- Laboratorio1/reto4/Reto4.java
+++ Laboratorio1/reto4/Reto4.java
@@ -1,8 +1,9 @@
 package Laboratorio1.reto4;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
 public class Reto4 {
      public static HashMap<String,Integer> convertirAHashMap(List<Map.Entry<String,Integer>> tuplas) {
@@ -16,6 +17,18 @@ public class Reto4 {
         }
         return resultado;
     }
+    public static Hashtable<String,Integer> convertirAHashtable(List<Map.Entry<String,Integer>> pares) {
+        Hashtable<String,Integer> resultado = new Hashtable<>();
+        for (Map.Entry<String,Integer> p : pares) {
+            String key = p.getKey();
+            int valor = p.getValue();
+            if(!resultado.containsKey(key)){
+                resultado.put(key,valor);
+            }
+        }
+        return resultado;
+    }
+
 
     public static void main(String[] args) {
         List<Map.Entry<String, Integer>> datos1 = List.of(
@@ -27,6 +40,8 @@ public class Reto4 {
 
         HashMap<String, Integer> resultadoHashMap = convertirAHashMap(datos1);
         System.out.println(resultadoHashMap);
+        Hashtable<String, Integer> resultado = convertirAHashtable(datos1);
+        System.out.println(resultado);
     }
 }
 
