f3f942394ec485c422133433a23b87e26ac6fc25
Valentina-33 <lozano.paula1021@gmail.com>
2026-02-05 19:58:11 -0500

    Implementando el filtro y organización para ambas estructuras

--- Laboratorio1/reto5/Reto5.java
+++ Laboratorio1/reto5/Reto5.java
@@ -3,11 +3,11 @@ package Laboratorio1.reto5;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.function.Consumer;
 
 public class Reto5 {
 
     public static Set<Integer> generarHashSetSinMultiplosDe3(Set<Integer> entrada) {
-        // requisito: stream().filter()
         return entrada.stream()
                 .filter(n -> n % 3 != 0)
                 .collect(java.util.stream.Collectors.toCollection(HashSet::new));
@@ -19,15 +19,25 @@ public class Reto5 {
                 .collect(java.util.stream.Collectors.toCollection(TreeSet::new));
     }
 
+    public static TreeSet<Integer> unirEnOrden(Set<Integer> a, Set<Integer> b) {
+        return java.util.stream.Stream.concat(a.stream(), b.stream())
+                .collect(java.util.stream.Collectors.toCollection(TreeSet::new));
+    }
+
+    public static void imprimirArena(Set<Integer> numeros) {
+        Consumer<Integer> imprimir = n -> System.out.println("Número en arena: " + n);
+        numeros.forEach(imprimir);
+    }
+
     public static void main(String[] args) {
-        
         Set<Integer> hashSetEntrada = Set.of(4, 9, 15, 7, 18, 21, 10, 5);
         Set<Integer> treeSetEntrada = Set.of(12, 3, 25, 10, 7, 30, 18, 4);
 
         Set<Integer> a = generarHashSetSinMultiplosDe3(hashSetEntrada);
         Set<Integer> b = generarTreeSetSinMultiplosDe5(treeSetEntrada);
 
-        System.out.println(a);
-        System.out.println(b);
+        TreeSet<Integer> unionOrdenada = unirEnOrden(a, b);
+
+        imprimirArena(unionOrdenada);
     }
 }
