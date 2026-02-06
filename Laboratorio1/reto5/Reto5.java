a322bbc32550c611611fd7334c80e57cb6f1eb2e
Valentina-33 <lozano.paula1021@gmail.com>
2026-02-05 19:37:04 -0500

    Añadiendo el filtro de múltiplos de 3 para TreeSet

--- Laboratorio1/reto5/Reto5.java
+++ Laboratorio1/reto5/Reto5.java
@@ -2,6 +2,7 @@ package Laboratorio1.reto5;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Reto5 {
 
@@ -12,12 +13,22 @@ public class Reto5 {
                 .collect(java.util.stream.Collectors.toCollection(HashSet::new));
     }
 
+    public static Set<Integer> generarTreeSetSinMultiplosDe5(Set<Integer> entrada) {
+        // requisito: stream().filter()
+        return entrada.stream()
+                .filter(n -> n % 5 != 0)
+                .collect(java.util.stream.Collectors.toCollection(TreeSet::new));
+    }
+
     public static void main(String[] args) {
         // Ejemplo Entrada del PDF
         Set<Integer> hashSetEntrada = Set.of(4, 9, 15, 7, 18, 21, 10, 5);
+        Set<Integer> treeSetEntrada = Set.of(12, 3, 25, 10, 7, 30, 18, 4);
 
         Set<Integer> a = generarHashSetSinMultiplosDe3(hashSetEntrada);
+        Set<Integer> b = generarTreeSetSinMultiplosDe5(treeSetEntrada);
 
         System.out.println(a);
+        System.out.println(b);
     }
 }
